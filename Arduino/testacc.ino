f43e0734ee9563a3c8a782a44672bf6553266cc5
Jay Shankar Gupta <jaygupta12300@gmail.com>
2018-01-13 00:16:39 +0530

    Add files via upload

---
+++ Arduino/testacc.ino
@@ -0,0 +1,26 @@
+//const int powerpin = 13;              // analog input pin 5 -- voltage
+int xpin = A3;                  // x-axis of the accelerometer
+int ypin = A2;                  // y-axis
+int zpin = A1;                  // z-axis (only on 3-axis models)
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+  //pinMode(powerpin, OUTPUT);
+  //digitalWrite(powerpin, HIGH);
+}
+
+void loop() 
+{
+  // print the sensor values:
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print(' ');
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print(' ');
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  // delay before next reading:
+  delay(100);
+}
---
+++ C#Code/onscreenarrows/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace onscreenarrows
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myport;
+        private string indata;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.ExStyle = 0x08000000;
+                return param;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            try {
+                myport = new SerialPort();
+                myport.BaudRate = 9600;
+                myport.PortName = "COM4";
+                myport.Open();
+                while (true)
+                {
+                    indata = myport.ReadLine();
+                    string[] str1 = indata.Split(' ');
+                    int x = Int32.Parse(str1[0]);
+                    int y = Int32.Parse(str1[1]);
+                    if (x < 350)
+                    {
+                        SendKeys.SendWait("{LEFT}");
+                    }
+                    else if (x > 400)
+                    {
+                        SendKeys.SendWait("{RIGHT}");
+                    }
+                    else { }
+                    if (y < 350)
+                    {
+                        SendKeys.SendWait("{DOWN}");
+                    }
+                    else if (y > 400)
+                    {
+                        SendKeys.SendWait("{SPACE}");
+                    }
+                    else { }
+                }
+            }
+            finally
+            {
+                myport.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myport.Close();
+        }
+    }
+}
